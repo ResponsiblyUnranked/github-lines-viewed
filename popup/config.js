9f796d362700e93f1cdadee8fa4f677ee369c821
Jamie McKernan <hjht2bff@duck.com>
2024-08-31 10:39:45 +0100

    Run prettier

--- popup/config.js
+++ popup/config.js
@@ -1,2 +1,6 @@
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+const popoverTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="popover"]',
+)
+const popoverList = [...popoverTriggerList].map(
+  (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl),
+)
